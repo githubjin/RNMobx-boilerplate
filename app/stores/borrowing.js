ad4b90a7ed56cb4ce8eaedfb52c4ed60748041b1
金继钊 <jinjizhao@83chedai.com>
2017-06-30 16:31:32 +0800

    jest merge

---
+++ app/stores/borrowing.js
@@ -0,0 +1,11 @@
+/**
+ *  借款详情
+ * @flow
+ */
+
+import React, { Component } from "react";
+import { observable, action } from "mobx";
+
+class Borrowing {
+    @observable 
+}
--- app/stores/schemas/borrowing.js
+++ app/stores/schemas/borrowing.js
@@ -4,25 +4,31 @@
  */
 import { normalize, schema } from "normalizr";
 
-const borrowing: schema.Entity = new schema.Entity("borrowings");
+const borrowing: schema.Entity = new schema.Entity("borrowings", {
+  application
+});
 
 const application: schema.Entity = new schema.Entity("application");
 
 const vehicle: schema.Entity = new schema.Entity("vehicle");
 
 const borrower: schema.Entity = new schema.Entity("borrower");
-
+// 社会关系
 const social_net_node: schema.Entity = new schema.Entity(
   "social_net__node",
   {},
   { idAttribute: "mobile" }
 );
-
 const social_net: schema.Entity = new schema.Entity("social_net", {
   colleagues: [social_net_node],
   friends: [social_net_node],
   emergencyContacts: [social_net_node],
   relatives: [social_net_node]
 });
-
-const property_item = new schema.Entity("property_item");
+// 资产
+const property = new schema.Entity("property");
+const properties = new schema.Entity("properties", {
+  houseProperties: [property],
+  financialAssets: [property],
+  cars: [property]
+});
